39fbe99be4859f83702133bb12466848bc6dd765
Prasath Govindaraju <govindarajuprasath@gmail.com>
2025-09-12 13:14:49 -0300

    Renamed Class to BankOfPrasGov

--- src/main/java/com/prazy/mc/bankofprasgov/BankOfPrasGovApplication.java
+++ src/main/java/com/prazy/mc/bankofprasgov/BankOfPrasGovApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BankOfPrazyApplication {
+public class BankOfPrasGovApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BankOfPrazyApplication.class, args);
+		SpringApplication.run(BankOfPrasGovApplication.class, args);
 	}
 
 }
